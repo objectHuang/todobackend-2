4ee1530b118bd85bec736328adc62d415a5e2a65
Jacob <jacob@Jacobs-MacBook-Pro.local>
2018-11-10 11:37:10 +0800

    release image

---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. /appenv/bin/activate
+pip download -d /build -r requirements_test.txt --no-input
+pip install --no-index -f /build -r requirements_test.txt
+exec $@
---
+++ src/todobackend/settings/release.py
@@ -0,0 +1,26 @@
+from base import *
+import os
+
+#Disable = True
+if os.environ.get('DEBUG'):
+    DEBUG = True
+else
+    DEBUG = False
+
+#Must be explicity specified when Debug is disabled
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
+
+
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    }
+}
+
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '/var/www/todobackend/static')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/var/www/todobackend/media')
